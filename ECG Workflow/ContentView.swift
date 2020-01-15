5a7cfe1cab61a952bbb14969fb356ced68230f14
Ray Wenderlich <raywenderlich>
2020-01-15 17:08:31 +0100

    Initial Commit

---
+++ ECG Workflow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ECG Workflow
+//
+//  Created by Yannick Börner on 15.01.20.
+//  Copyright © 2020 Berlin Institute of Health. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ECG WorkflowTests/ECG_WorkflowTests.swift
@@ -0,0 +1,34 @@
+//
+//  ECG_WorkflowTests.swift
+//  ECG WorkflowTests
+//
+//  Created by Yannick Börner on 15.01.20.
+//  Copyright © 2020 Berlin Institute of Health. All rights reserved.
+//
+
+import XCTest
+@testable import ECG_Workflow
+
+class ECG_WorkflowTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
